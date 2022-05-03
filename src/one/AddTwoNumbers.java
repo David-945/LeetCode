0df651af6c6db5b80c570020777223a4fa4352eb
msc <msc7868@163.com>
2022-05-03 21:40:15 +0800

    提交了第一二题

---
+++ src/one/AddTwoNumbers.java
@@ -0,0 +1,105 @@
+package one;
+
+//给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。 
+//
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。 
+//
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：l1 = [2,4,3], l2 = [5,6,4]
+//输出：[7,0,8]
+//解释：342 + 465 = 807.
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：l1 = [0], l2 = [0]
+//输出：[0]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+//输出：[8,9,9,9,0,0,0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 每个链表中的节点数在范围 [1, 100] 内 
+// 0 <= Node.val <= 9 
+// 题目数据保证列表表示的数字不含前导零 
+// 
+// Related Topics 递归 链表 数学 
+// 👍 8012 👎 0
+/*
+第二题两数相加
+*/
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //这里定义新节点为指针，为了返回的时候使用这个链表，但是这个链表的头节点为
+        ListNode root = new ListNode(0);
+        ListNode cursor = root;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1Val = l1 != null ? l1.val : 0;
+            int l2Val = l2 != null ? l2.val : 0;
+            int sumVal = l1Val + l2Val + carry;
+            carry = sumVal / 10;
+
+            //将求和数赋值给新链表的节点，其实这里就是关于对象的内存地址的问题
+            //cursor和root都是指向同一个内存地址，所以cursor改变，也会使root链表中的内存地址发生改变
+            ListNode sumNode = new ListNode(sumVal % 10);
+            cursor.next = sumNode;
+            //将新链表的节点后移
+            cursor = sumNode;
+
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+        return root.next;
+
+    }
+
+    public static void main(String[] args) {
+        AddTwoNumbers n = new AddTwoNumbers();
+        // 342
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        // 342
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        n.addTwoNumbers(l1, l2);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/one/Solution.java
@@ -0,0 +1,45 @@
+package one;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author msc
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/5/3 21:37
+ * 第一题两数之和
+ */
+public class Solution {
+    // public int[] twoSum(int[] nums, int target) {
+    //     int[] ints = new int[2];
+    //     for (int i = 0; i < nums.length-1; i++) {
+    //         for(int j = i+1; j <nums.length;j++){
+    //             if(nums[i] + nums[j] == target){
+    //                 ints[0] = nums[i];
+    //                 ints[1] = nums[j];
+    //                 return ints;
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+    public static void main(String[] args){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        Solution s = new Solution();
+        int[] i = s.twoSum(nums,target);
+        System.out.println(Arrays.toString(i));
+    }
+}
